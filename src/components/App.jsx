fe9571d60ee228c273d9059b04029f5c5eec255b
Philip Winchester <philipwinc@gmail.com>
2016-11-22 14:41:31 -0500

    copied npm react-drawable-canvas to local file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+// import DrawableCanvas from 'react-drawable-canvas';
 import DrawCanvas from './DrawCanvas';
-import DrawableCanvas from 'react-drawable-canvas';
 import './normalize.css';
 import './App.css';
 
 export default class App extends Component {
 
-  // handlePrint() {
-  //   console.log(this.state);
-  // }
-
   render() {
     return (
       <div>
         <h1>Canvas Demo</h1>
-        <DrawableCanvas />
-        <DrawCanvas />
+        <DrawCanvas
+          brushColor={'#000000'}
+          lineWidth={4}
+          canvasStyle={{
+            backgroundColor: '#FFFFFF',
+            cursor: 'pointer',
+          }}
+          clear={false}
+        />
         <button> See State </button>
       </div>
     );
