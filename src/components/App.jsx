7f59b1140881da1049c1f44bd0f21ed690928427
Philip Winchester <philipwinc@gmail.com>
2016-11-22 10:01:55 -0500

    created db and schema file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './normalize.css';
 import './App.css';
-import Form from './Form.jsx';
-import Search from './Search.jsx';
-import Database from './Database.jsx';
 
 class App extends Component {
-
   render() {
     return (
       <div>
