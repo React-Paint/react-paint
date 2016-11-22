a8649761ad42a5f5f0766a3a95e190b16d675b64
Philip Winchester <philipwinc@gmail.com>
2016-11-22 17:00:12 -0500

    updated canvas state

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Simple React application for MS-Paint enthusiasts
+
+- DrawCanvas Component is a clone of https://github.com/jonhni/react-drawable-canvas, but reworked slightly to fit with our project.
--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,11 @@ BEGIN;
 
 DROP TABLE IF EXISTS canvas;
 
-CREATE TABLE canvas(
+CREATE TABLE canvas (
   id SERIAL PRIMARY KEY,
   title VARCHAR NOT NULL,
   description TEXT,
-  drawing TEXT NOT NULL
+  drawing  INTEGER ARRAY
 );
 
 COMMIT;
