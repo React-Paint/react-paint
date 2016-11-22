c25415576a09f93405d833e6656df8c5ea80003a
Philip Winchester <philipwinc@gmail.com>
2016-11-22 10:55:44 -0500

    imported canvasHelper file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+// import CanvasHelper from '../helpers/CanvasHelper';
 import './normalize.css';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Set up React</h1>
+        <canvas className="cvs" />
+        <button className="publish">Publish</button>
       </div>
     );
   }
 }
-
-export default App;
