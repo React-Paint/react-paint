a8ea7864b238abc7c13355b475a2b68b48d69b67
Philip Winchester <philipwinc@gmail.com>
2016-11-23 09:30:52 -0500

    added schema for users table

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,20 @@
 BEGIN;
 
 DROP TABLE IF EXISTS canvas;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE canvas (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR NOT NULL,
-  description TEXT,
-  drawing  INTEGER ARRAY
+  id            SERIAL PRIMARY KEY,
+  title         VARCHAR NOT NULL,
+  description   TEXT,
+  drawing       INTEGER ARRAY
+);
+
+CREATE TABLE user (
+  id            SERIAL PRIMARY KEY,
+  username      VARCHAR NOT NULL,
+  password      VARCHAR(64) NOT NULL,
+  email         VARCHAR
 );
 
 COMMIT;
