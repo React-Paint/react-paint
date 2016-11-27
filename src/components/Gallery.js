02c0e4789c0515da65990145cf47b35d4cd07e35
Philip Winchester <philipwinc@gmail.com>
2016-11-27 13:53:51 -0500

    fixed merge conflicts with users branch

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,64 +2,20 @@ import React, { Component } from 'react';
 import GalleryItem from './GalleryItem';
 import './App.css';
 
-<<<<<<< HEAD
-// This way can use edit button
-export default class Gallery extends Component {
-  render() {
-    console.log(this.props.drawings);
-    const drawing = this.props.drawings.map((canv, ind) => {
-      return (
-        <GalleryItem
-          key={ind}
-          title={canv.title}
-          desc={canv.description}
-          canvas={canv.drawing}
-          id={`canvas${canv.id}`}
-          editCanvas={() => this.props.editCanvas(canv.id)}
-=======
-// Now live refreshes and edit button works
 export default class Gallery extends Component {
   render() {
     console.log('rendering');
-      const drawing = Object.keys(this.props.drawings)
-        .map((canvID, ind) => (
-          <GalleryItem
-            key={ind}
-            title={this.props.drawings[canvID].title}
-            desc={this.props.drawings[canvID].description}
-            canvas={this.props.drawings[canvID].drawing}
-            id={`canvas${this.props.drawings[canvID].id}`}
-            editCanvas={() => this.props.editCanvas(this.props.drawings[canvID].id)}
-          />
-        ));
-    return (
-      <div className="gallery-div">
-        <h1>Gallery</h1>
-        {drawing}
-      </div>
-    );
-  }
-}
-
-/*
-// This way can live update
-const Gallery = props => {
-
-  const generateGalleryList = drawings =>
-    Object.keys(drawings)
-      // .filter(canvID => props.filter(drawings[canvID]))
-      .map((canvID, i) => (
+    const drawing = Object.keys(this.props.drawings)
+      .map((canvID, ind) => (
         <GalleryItem
-          key={i}
-          title={drawings[canvID].title}
-          desc={drawings[canvID].description}
-          canvas={drawings[canvID].drawing}
-          id={`canvas${canvID}`}
-          editCanvas={() => props.editCanvas(canvID.id)}
->>>>>>> users
+          key={ind}
+          title={this.props.drawings[canvID].title}
+          desc={this.props.drawings[canvID].description}
+          canvas={this.props.drawings[canvID].drawing}
+          id={`canvas${this.props.drawings[canvID].id}`}
+          editCanvas={() => this.props.editCanvas(this.props.drawings[canvID].id)}
         />
-      )
-    });
+    ));
     return (
       <div className="gallery-div">
         <h1>Gallery</h1>
@@ -68,36 +24,3 @@ const Gallery = props => {
     );
   }
 }
-
-
-<<<<<<< HEAD
-// // This way can live update
-// const Gallery = props => {
-//
-//   const generateGalleryList = drawings =>
-//     Object.keys(drawings)
-//       // .filter(canvID => props.filter(drawings[canvID]))
-//       .map((canvID, i) => (
-//         <GalleryItem
-//           key={i}
-//           title={drawings[canvID].title}
-//           desc={drawings[canvID].description}
-//           canvas={drawings[canvID].drawing}
-//           id={`canvas${canvID}`}
-//           editCanvas={() => props.editCanvas(canvID.id)}
-//         />
-//     ));
-//
-//   return (
-//     <div className="gallery-div">
-//       <h1>Gallery</h1>
-//       {generateGalleryList(props.drawings)}
-//     </div>
-//   );
-// };
-//
-// export default Gallery;
-=======
-export default Gallery;
-*/
->>>>>>> users
