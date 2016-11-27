658ecf943a24b37d1dbdee3dc37c07e5a0bf5d4b
Philip Winchester <philipwinc@gmail.com>
2016-11-26 23:17:29 -0500

    syntax update in Gallery Component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import GalleryItem from './GalleryItem';
 import './App.css';
 
+export default class Gallery extends Component {
+  render() {
+    const drawing = this.props.drawings.map((canv, ind) => {
+      return (
+        <GalleryItem
+          key={ind}
+          title={canv.title}
+          desc={canv.description}
+          canvas={canv.drawing}
+          id={`canvas${canv.id}`}
+          editCanvas={() => this.props.editCanvas(canv.id)}
+        />
+      )
+    });
+    return (
+      <div className="gallery-div">
+        <h1>Gallery</h1>
+        {drawing}
+      </div>
+    );
+  }
+}
+
+/*
 const Gallery = props => {
   const generateGalleryList = drawings =>
     Object.keys(drawings)
@@ -12,7 +36,7 @@ const Gallery = props => {
           desc={drawings[canvID].description}
           canvas={drawings[canvID].drawing}
           id={`canvas${canvID}`}
-          editCanvas={() => this.props.editCanvas(canvID)}
+          editCanvas={(canvID) => props.editCanvas(canvID.id)}
         />
     ));
 
@@ -25,3 +49,4 @@ const Gallery = props => {
 };
 
 export default Gallery;
+*/
