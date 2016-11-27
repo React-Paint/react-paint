da423d13533eb8e7d5e98e3b400f4033f5a2567d
matt keigwin <mkeigwin@bates.edu>
2016-11-27 10:52:06 -0500

    background to saved images

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -4,13 +4,21 @@ import './App.css';
 export default class GalleryItem extends Component {
 
   render() {
+    const banana = this.props.URL;
+    const style = {
+      background: 'url(' + banana + ')',
+      height: 400,
+      width: 600,
+    };
     return (
       <div className="drawing-card">
         <h4>{this.props.title}</h4>
         <p>
           {this.props.desc}
         </p>
-        <img src={this.props.canvas} alt={this.props.title} id={this.props.id}/>
+        <div style={style}>
+          <img src={this.props.canvas} alt={this.props.title} id={this.props.id} />
+        </div>
         <button onClick={this.props.editCanvas}>Edit</button>
       </div>
     );
