5a37a4c3eb2728e13f0398e878f0b4c7f7bc8ab1
Philip Winchester <philipwinc@gmail.com>
2016-11-27 00:00:29 -0500

    debugging live update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -77,8 +77,10 @@ export default class App extends Component {
     AjaxFunctions.addDrawing(canvasData)
       .then(drawing => {
         const newState = {...this.state.drawings};
-        console.log(newState);
+        console.log(newState.length);
         newState[drawing.id] = drawing;
+        console.log(newState);
+
         this.setState({
           drawings: newState
         })
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,8 +3,8 @@ import GalleryItem from './GalleryItem';
 import './App.css';
 
 export default class Gallery extends Component {
-  render() {
-    const drawing = this.props.drawings.map((canv, ind) => {
+  generateGalleryList() {
+    Object.keys(this.props.drawings).map((canv, ind) => {
       return (
         <GalleryItem
           key={ind}
@@ -16,37 +16,38 @@ export default class Gallery extends Component {
         />
       )
     });
+  }
+  render() {
     return (
       <div className="gallery-div">
         <h1>Gallery</h1>
-        {drawing}
+        {this.generateGalleryList()}
       </div>
     );
   }
 }
 
-/*
-const Gallery = props => {
-  const generateGalleryList = drawings =>
-    Object.keys(drawings)
-      .map((canvID, i) => (
-        <GalleryItem
-          key={i}
-          title={drawings[canvID].title}
-          desc={drawings[canvID].description}
-          canvas={drawings[canvID].drawing}
-          id={`canvas${canvID}`}
-          editCanvas={(canvID) => props.editCanvas(canvID.id)}
-        />
-    ));
 
-  return (
-    <div className="gallery-div">
-      <h1>Gallery</h1>
-      {generateGalleryList(props.drawings)}
-    </div>
-  );
-};
-
-export default Gallery;
-*/
+// const Gallery = props => {
+//   const generateGalleryList = drawings =>
+//     Object.keys(drawings)
+//       .map((canvID, i) => (
+//         <GalleryItem
+//           key={i}
+//           title={drawings[canvID].title}
+//           desc={drawings[canvID].description}
+//           canvas={drawings[canvID].drawing}
+//           id={`canvas${canvID}`}
+//           editCanvas={(canvID) => props.editCanvas(canvID.id)}
+//         />
+//     ));
+//
+//   return (
+//     <div className="gallery-div">
+//       <h1>Gallery</h1>
+//       {generateGalleryList(props.drawings)}
+//     </div>
+//   );
+// };
+//
+// export default Gallery;
