521ae1e3be11fce68320b22ea5880b1595ac2f6c
Philip Winchester <philipwinc@gmail.com>
2016-11-28 16:35:07 -0500

    updated linter and package dependencies

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 'use strict';
-require('dotenv').config({ silent: true });
 const express     = require('express');
 const logger      = require('morgan');
 const path        = require('path');
 const bodyParser  = require('body-parser');
 
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+
 const app         = express();
 const PORT        = process.argv[2] || process.env.port || 3000;
 
-app.use(logger('dev'));
+app.use(logger(isDev ? 'dev' : 'common'));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
