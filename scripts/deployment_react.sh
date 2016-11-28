286e9dc8e951d141bedb075ec2dd67ce6675ffba
Philip Winchester <philipwinc@gmail.com>
2016-11-28 15:08:04 -0500

    uncommented

--- scripts/deployment_react.sh
+++ scripts/deployment_react.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 # @author Jason Seminara for General Assembly, 2016
 # NPM has some weird ideas about which scripts it likes to fire off when deploying.
 # Let's just do exactly what we want to deploy this React app.
