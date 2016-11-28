2363748ed8bca5e28de092d9eaa0a2627adf84ad
Philip Winchester <philipwinc@gmail.com>
2016-11-28 15:16:35 -0500

    uncommented

--- scripts/deployment_cleanup.sh
+++ scripts/deployment_cleanup.sh
@@ -2,4 +2,4 @@
 # @author Jason Seminara for General Assembly, 2016
 # @desc Deployment should be as slim as possible, and with any identifying remarks or traces removed (i.e., we don't need to tell anyone how we built our app when it's deployed ;).
 
-rm -rfs db src scripts starter_resources lessons webpack.config.js .babelrc .gitignore README.md
+rm -rf db src scripts webpack.config.js .babelrc .gitignore README.md
